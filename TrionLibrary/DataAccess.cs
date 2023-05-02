4671de79e2b0e83b2e9a30fb280942535eee0440
Flying Phoenix <81469821+fIyingPhoenix@users.noreply.github.com>
2023-05-02 21:44:43 +0200

    02.05.2023 - 01
    New TCP!
    Linux Support!
    Windows Support!
    Do we need Mac?
    
    The new one is web based. So it can run on Linux Server and Windows Server with no UI.
    This is for me to learn better HTML and CSS.

---
+++ TrionLibrary/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using System.Linq;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TrionLibrary
+{
+    public class MySQLDataAccess
+    {
+        public async Task<List<T>> LodaData<T,U> (string sql ,U parameters, string connectionString)
+        {
+            using (IDbConnection con = new MySqlConnection (connectionString))
+            {
+                var rows = await con.QueryAsync<T>(sql, parameters);
+                return rows.ToList();
+            }
+        }
+        public Task SaveData<T> (string sql,T parameters,string connectionString)
+        {
+            using (IDbConnection con = new MySqlConnection(connectionString))
+            {
+                return con.ExecuteAsync(sql, parameters);
+            }
+        
+        }
+    }
+}
---
+++ TrionLibrary/SystemWatcher.cs
@@ -0,0 +1,65 @@
+﻿using Org.BouncyCastle.Bcpg;
+using System;
+using System.Diagnostics;
+
+namespace TrionLibrary
+{
+    public static class SystemWatcher
+    {
+        //World Variables 
+        public static string WorldName { get;set; }
+        public static int WorldRam { get; set; }
+        public static int WorldPID { get; set; }
+        //Auth Variables 
+        public static string AuthName{ get;set; }
+        public static int AuthRam { get; set; }
+        public static int AuthPID { get; set; }
+        //Trion Control Panel Variables
+        public static string DatabaseName { get; set; }
+        public static string TCPName { get; set; }
+        public static int totalRam {get; set; }
+        //
+        public static bool WorldRuning()
+        {
+            WorldName = "";
+            Process[] pname = Process.GetProcessesByName(WorldName);
+            if (pname.Length <= 0)
+                return false;
+            else
+                return true;
+        }
+        public static bool WorldKilled()
+        {
+            bool succes = false;
+            WorldName = "";
+            foreach (var process in Process.GetProcessesByName(WorldName))
+            {
+                try { process.Kill(); succes = true; }
+                catch (Exception ex){ succes = false;}
+            }
+            return succes;
+        }
+        public static bool AuthRuning()
+        {
+            WorldName = "";
+            Process[] pname = Process.GetProcessesByName(AuthName);
+            if (pname.Length <= 0)
+                return false;
+            else
+                return true;
+        }
+        public static bool DatabaseRuning()
+        {
+            return false;
+        }
+        public static bool TCPRuning()
+        {
+            TCPName = "TrionControlPanel";
+            Process[] pname = Process.GetProcessesByName(TCPName);
+            if (pname.Length <= 0)
+                return false;
+            else
+                return true;
+        }
+    }
+}
