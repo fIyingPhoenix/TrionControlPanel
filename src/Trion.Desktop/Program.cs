e248ceefc9b4fcbaef1d6cf9963f06da6e6ee2e5
fIyingPhoenix <andrei081993@gmail.com>
2025-10-12 15:18:05 +0200

    chore: complete Category 1.1 – Project Initialization #32

---
+++ src/Trion.Desktop/Program.cs
@@ -0,0 +1,60 @@
+﻿using Photino.NET;
+using System.Drawing;
+using System.Text;
+
+namespace Trion.Desktop
+{
+    //NOTE: To hide the console window, go to the project properties and change the Output Type to Windows Application.
+    // Or edit the .csproj file and change the <OutputType> tag from "WinExe" to "Exe".
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Window title declared here for visibility
+            string windowTitle = "Trion Control Panel";
+
+            // Creating a new PhotinoWindow instance with the fluent API
+            var window = new PhotinoWindow()
+                .SetTitle(windowTitle)
+                // Resize to a percentage of the main monitor work area
+                .SetUseOsDefaultSize(false)
+                .SetSize(new Size(1024, 800))
+                // Center window in the middle of the screen
+                .Center()
+                // Users can resize windows by default.
+                // Let's make this one fixed instead.
+                .SetResizable(false)
+                .RegisterCustomSchemeHandler("app", (object sender, string scheme, string url, out string contentType) =>
+                {
+                    contentType = "text/javascript";
+                    return new MemoryStream(Encoding.UTF8.GetBytes(@"
+                        (() =>{
+                            window.setTimeout(() => {
+                                alert(`🎉 Dynamically inserted JavaScript.`);
+                            }, 1000);
+                        })();
+                    "));
+                })
+                // Most event handlers can be registered after the
+                // PhotinoWindow was instantiated by calling a registration 
+                // method like the following RegisterWebMessageReceivedHandler.
+                // This could be added in the PhotinoWindowOptions if preferred.
+                .RegisterWebMessageReceivedHandler((object sender, string message) =>
+                {
+                    var window = (PhotinoWindow)sender;
+
+                    // The message argument is coming in from sendMessage.
+                    // "window.external.sendMessage(message: string)"
+                    string response = $"Received message: \"{message}\"";
+
+                    // Send a message back the to JavaScript event handler.
+                    // "window.external.receiveMessage(callback: Function)"
+                    window.SendWebMessage(response);
+                })
+                .Load("wwwroot/index.html"); // Can be used with relative path strings or "new URI()" instance to load a website.
+
+            window.WaitForClose(); // Starts the application event loop
+        }
+    }
+}
---
+++ tests/Trion.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Trion.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
